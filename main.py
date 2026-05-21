f5ae574daf61c4b589da0b7d3d39e74a6423a2d4
JoseLuisSR <jose.luis.sastoque.rey@gmail.com>
2026-05-21 15:36:08 -0500

    Create infrastructure package and split adaperts to api and llm.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("adapters.web.app:app", host="0.0.0.0", port=8080, reload=True)
+    uvicorn.run("infrastructure.api.app:app", host="0.0.0.0", port=8080, reload=True)
