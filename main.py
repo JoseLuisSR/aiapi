b6817df4aa613309908fec05885e74f36186dbe5
JoseLuisSR <jose.luis.sastoque.rey@gmail.com>
2026-05-28 16:20:09 -0500

    Fix binding problem identify by Ruff.

--- main.py
+++ main.py
@@ -23,4 +23,4 @@ if __name__ == "__main__":
     EXAMPLES
     >>> python main.py
     """
-    uvicorn.run("infrastructure.api.app:app", host="0.0.0.0", port=8080, reload=True)
+    uvicorn.run("infrastructure.api.app:app", host="127.0.0.1", port=8080, reload=True)
